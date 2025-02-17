b9f4f87a1fba45d5012f1abf261e17e8829d10bc
TuanDHH <haituan@edufree.win>
2025-02-17 10:17:21 +0700

    commit conflict

---
+++ API/GO.API/Controllers/NxLogController.cs
@@ -0,0 +1,46 @@
+﻿using Com.Gosol.Models;
+using Com.Gosol.VHTT.API.Formats;
+using Com.Gosol.VHTT.BUS.HeThong;
+using Com.Gosol.VHTT.Models.HeThong;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Memory;
+using System.Text;
+
+namespace GO.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class NxLogController : ControllerBase
+    {
+        private SystemLogBUS _SystemLogBUS;
+        
+        [HttpPost]
+        [Route("Log")]
+        public async Task<IActionResult> LogMessage([FromBody] NXCamera request)
+        {
+            _SystemLogBUS = new SystemLogBUS();
+            if (request == null || request.Type == 0)
+            {
+                return BadRequest("Invalid input data.");
+            }
+
+            try
+            {
+                var model = new SystemLogModel();
+                model.CanBoID = 1;
+                model.LogInfo = request.Code + " | " + request.Type;
+                model.LogType = 1;
+                model.LogTime = DateTime.Now;
+                _SystemLogBUS.Insert(model);
+                return Ok("Log written successfully.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error writing log: {ex.Message}");
+            }
+        }
+    }
+}
